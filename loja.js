b71c63a8af1baee24d35211aac9ab71195567cdc
Nasiasene-dev <nasiasenebr@gmail.com>
2026-09-25 15:32:09 -0300

    Pequenas alterações para mudar o botão de sidebar com java script e um pouco de media querry

--- loja.js
+++ loja.js
@@ -1,9 +1,27 @@
-console.log("javascript funcionando")
 
 let botaoSidebar = document.getElementById("botao-sidebar")
 let sidebar = document.getElementById("sidebar");
+let iconeSidebar = document.getElementById("icone-sidebar")
 
 botaoSidebar.addEventListener("click", function(){
+
     console.log("botão clicado")
+
     sidebar.classList.toggle("aberta")
-botaoSidebar.classList.toggle("aberta")});
+    botaoSidebar.classList.toggle("aberta")
+
+    if (botaoSidebar.classList.contains("aberta")) {
+
+        iconeSidebar.classList.remove("bi-caret-left-square")
+        iconeSidebar.classList.add("bi-x-circle")
+
+    } else {
+
+        iconeSidebar.classList.remove("bi-x-circle")
+        iconeSidebar.classList.add("bi-caret-left-square")
+
+    }
+
+});
+
+
